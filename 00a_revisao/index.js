bd04b5cbaf641434c2f6c8ee4b4f9121c6306246
denizafontineles <denizafontineles@outlook.com>
2019-10-28 13:31:44 -0300

    resolvendo exercicio proposto

--- 00a_revisao/index.js
+++ 00a_revisao/index.js
@@ -1,7 +1,41 @@
 // 1.
-// [] adicionar a tag <script> no html
-// [] verificar se está lendo index.js
-// [] alterar cor de <body>
-// [] escutar o clique do botão
-// [] criar função de alterar a cor usando rgb()
-// [] usar Math.random() e Math.floor() ou parseInt()
+// [X] adicionar a tag <script> no html
+// [X] verificar se está lendo index.js
+// [X] alterar cor de <body>
+// [X] escutar o clique do botão
+// [X] criar função de alterar a cor usando rgb()
+// [X] usar Math.random() e Math.floor() ou parseInt()
+
+// console.log('Olá');
+
+const body = document.body;
+const escutaClick = document.getElementById('troca-cor');
+// const n = Math.random()
+// const n2 = n * 255
+// const int = parseInt(n2)
+
+// console.log('Numero:', n)
+// console.log('Numero2:', n2)
+// console.log('Int:', int)
+
+
+escutaClick.addEventListener('click', mudaCor);
+
+function geraInteiro() {
+    const n = parseInt(Math.random() * 255);
+    return n;
+}
+
+
+function mudaCor() {
+    // const rAleatorio = parseInt(Math.random() * 255)
+    // const bAleatorio = parseInt(Math.random() * 255)
+    // const gAleatorio = parseInt(Math.random() * 255)
+
+    const rAleatorio = geraInteiro();
+    const bAleatorio = geraInteiro();
+    const gAleatorio = geraInteiro();
+
+    body.style.backgroundColor = 'rgb(' + rAleatorio + ',' + gAleatorio + ',' + bAleatorio + ')';
+    console.log('rgb(' + rAleatorio + ',' + bAleatorio + ',' + rAleatorio + ')')
+}
--- 00b_revisao/index.js
+++ 00b_revisao/index.js
@@ -6,9 +6,37 @@
 // [] criar função de alterar a cor usando rgb()
 // [] usar Math.random() e Math.floor() ou parseInt()
 
+const body = document.body;
+const escutaClick = document.getElementById('troca-cor');
+
+escutaClick.addEventListener('click', () => {
+    const rAleatorio = geraInteiro();
+    const bAleatorio = geraInteiro();
+    const gAleatorio = geraInteiro();
+
+    body.style.backgroundColor = 'rgb(' + rAleatorio + ',' + gAleatorio + ',' + bAleatorio + ')';
+    console.log('rgb(' + rAleatorio + ',' + bAleatorio + ',' + rAleatorio + ')')
+
+});
+
+// function geraInteiro() {
+//     const n = parseInt(Math.random() * 255);
+//     return n;
+// }
+
+
+// function mudaCor() {
+// const rAleatorio = parseInt(Math.random() * 255)
+// const bAleatorio = parseInt(Math.random() * 255)
+// const gAleatorio = parseInt(Math.random() * 255)
+
+
 // ------------------------------------------------------------------------
 // 2. melhorando o código
-// [] remover repetição de código
-// [] anonymous function
-// [] arrow function
-// [] template string
+// [X] remover repetição de código
+// [X] anonymous function (Quando ela não precisa ser nomeada)
+
+// [X] arrow function ( é => no lugar da função)
+geraInteiro = n => parseInt(Math.random() * n);
+
+// [] template strings
